2f1b2cd33c1445a9896441dca6e4ec6d8c772b04
TodayNightt <sd.teoh.tsd@gmail.com>
2025-07-28 12:21:33 +0800

    ammendment for 7349c155ac3177ada2f7562911cb67b02c001a64

--- crates/services/video_converter_iced/src/assets/mod.rs
+++ crates/services/video_converter_iced/src/assets/mod.rs
@@ -22,13 +22,20 @@ pub fn ffmpeg_instance() -> &'static PathBuf {
         use std::fs::File;
         use std::io::Write;
 
+        let ffmpeg_name = if cfg!(target_os = "windows") {
+            "ffmpeg.exe"
+        } else {
+            "ffmpeg"
+        };
+
         let temp_dir = std::env::temp_dir();
-        let ffmpeg_path = temp_dir.join("ffmpeg.exe");
+
+        let ffmpeg_path = temp_dir.join(ffmpeg_name);
 
         if !ffmpeg_path.exists() {
             {
                 let mut file = File::create(&ffmpeg_path).unwrap();
-                file.write_all(&Binaries::get("ffmpeg").unwrap().data)
+                file.write_all(&Binaries::get(ffmpeg_name).unwrap().data)
                     .unwrap();
             }
         }
